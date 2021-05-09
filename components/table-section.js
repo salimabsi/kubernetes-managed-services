a3d704d4bd2af008b333367dbb330985acb3be46
salimabsi0 <salimabsifull@gmail.com>
2021-05-09 23:11:45 +0300

    Fix section title

--- components/table-section.js
+++ components/table-section.js
@@ -4,7 +4,9 @@ export default function ({ sectionData }) {
 
     return (
         <>
-            <div className="mt-20 p-2 text-2xl text-left text-blueGray-900">{sectionData.title}</div>
+            <div className="mt-20 mb-10 p-2 text-2xl text-left text-blueGray-900 relative whitespace-nowrap">
+                <span className="absolute">{sectionData.title}</span>
+            </div>
             {sectionData.rows.map((row, index) => <Row key={index} rowEntries={row} />)}
         </>
     )
