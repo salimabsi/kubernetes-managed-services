6ce44025588e2a1f40df664afd8c0a73000c37d2
salimabsi0 <salimabsifull@gmail.com>
2021-05-21 19:00:19 +0300

    Make the section title sticky

--- components/desktop-view/table-section.js
+++ components/desktop-view/table-section.js
@@ -2,9 +2,14 @@ import Cell from '../shared/cell';
 
 export default function TableSection({ sectionData }) {
 
+    const scrollToLeft = () => window.scrollTo({ left: 0, behavior: 'smooth' });
+
     return (
         <>
-            <div><span>{sectionData.title}</span></div>
+            <div className="mt-16"></div>
+            <tr onClick={scrollToLeft} className="sticky-row">
+                <td className="sticky-title" colSpan="99"><span>{sectionData.title}</span></td>
+            </tr>
             {sectionData.rows.map((row, index) => (
                 <tr key={index}>
                     <td>{row[0]}</td>
@@ -14,4 +19,3 @@ export default function TableSection({ sectionData }) {
         </>
     )
 }
-
