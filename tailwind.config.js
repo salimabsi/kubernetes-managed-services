e52869e589672fc95131bb9273ff955615c0a076
salimabsi0 <salimabsifull@gmail.com>
2021-09-19 08:55:11 +0300

    Clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
   theme: {
     extend: {
       colors: {
-        lightBlue: colors.lightBlue,
-        cyan: colors.cyan,
         blueGray: colors.blueGray,
-        trueGray: colors.trueGray,
-        coolGray: colors.coolGray,
-        warmGray: colors.warmGray,
-        orange: colors.orange,
-        amber: colors.amber,
         lime: colors.lime,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        violet: colors.violet,
         fuchsia: colors.fuchsia,
-        rose: colors.rose,
       },
       fontFamily: {
-        'Inter': ['Inter'],
+        'Inter': ['Inter', ...defaultTheme.fontFamily.sans],
         'work-sans': ['Work Sans', 'sans-serif'],
       },
     },
